1f8b2707f02867703412968e138fa857472a13a8
DzarZaki <dzarzaki2@gmail.com>
2024-11-22 05:13:09 +0700

    Menambahkan box dengan background putih dan sudut membulat

--- app/src/main/java/com/example/pertemuanke8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuanke8/ui/view/screen/RencanaStudyView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
@@ -75,5 +76,16 @@ fun RencanaStudyView(
                 )
             }
         }
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize(),
+        )
     }
 }

5b298c13d05c0b9d3d685a43ae9e4e29f1e26dd8
DzarZaki <dzarzaki2@gmail.com>
2024-11-22 05:09:18 +0700

    Menambahkan daftar opsi mata kuliah

--- app/src/main/java/com/example/pertemuanke8/data/MataKuliah.kt
+++ app/src/main/java/com/example/pertemuanke8/data/MataKuliah.kt
@@ -1,4 +1,11 @@
 package com.example.pertemuanke8.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemograman Aplikasi Mobile Lanjut ",
+        "Pemograman Aplikasi Web Framework",
+        "prototyping",
+        "Aplikasi multimedia",
+        "Computer vission"
+    )
 }

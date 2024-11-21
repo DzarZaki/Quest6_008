c897123b3c416921360b2a2b2b80a552dd4a61ce
DzarZaki <dzarzaki2@gmail.com>
2024-11-22 05:36:34 +0700

    Menambahkan teks untuk setiap opsi kelas

--- app/src/main/java/com/example/pertemuanke8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuanke8/ui/view/screen/RencanaStudyView.kt
@@ -135,7 +135,7 @@ fun RencanaStudyView(
                             selected = pilihankelas == data,
                             onClick = { pilihankelas = data }
                         )
-
+                            Text(data)
                         }
                     }
                 }

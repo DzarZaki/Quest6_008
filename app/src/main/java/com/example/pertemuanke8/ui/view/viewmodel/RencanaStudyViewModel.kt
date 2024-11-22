61f5ab2ee18c69709bd8b091f1e8816dad0ae291
DzarZaki <dzarzaki2@gmail.com>
2024-11-22 09:36:23 +0700

    Menambahkan ViewModel untuk mengelola data rencana studi

--- app/src/main/java/com/example/pertemuanke8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuanke8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.pertemuanke8.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import com.example.pertemuanke8.model.RencanaStudi
+import kotlinx.coroutines.flow.StateFlow
+
+class RencanaStudyViewModel : viewModel() {
+    private val _krsState = mutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 }
